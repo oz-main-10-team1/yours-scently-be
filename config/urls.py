077383811705ca34f2c56eea02bd4c53e45e85a3
kyukyu300 <kyujin1252@gmail.com>
2025-07-27 01:40:16 +0900

    📝 docs: 조건문 추가 (SCENTLY-16)

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-# from django.contrib import admin
+from django.conf import settings
 from django.urls import URLPattern, URLResolver, path
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -23,8 +22,11 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
-urlpatterns: list[URLPattern | URLResolver] = [
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-]
+urlpatterns: list[URLPattern | URLResolver] = []
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+        path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
