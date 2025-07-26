6e7d2b46b76122516a0968d1e138556589942cc6
kyukyu300 <kyujin1252@gmail.com>
2025-07-27 01:19:58 +0900

    📝 docs: 스웨거 문서 추가 (SCENTLY-16)
    스웨거 문서 경로 추가 및 simplejwt, django-filter 라이브러리 설치

--- config/urls.py
+++ config/urls.py
@@ -17,5 +17,14 @@ Including another URLconf
 
 # from django.contrib import admin
 from django.urls import URLPattern, URLResolver, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
-urlpatterns: list[URLPattern | URLResolver] = []
+urlpatterns: list[URLPattern | URLResolver] = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+]
