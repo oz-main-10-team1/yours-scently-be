8c0be30c0e57c0d00eba3eec5e15d49061854eae
yeon tae <g9060g9060@gmail.com>
2025-07-28 14:09:23 +0900

    ♻️ refactor : 코드 수정 (SCENTLY-20)

--- apps/users/models/note.py
+++ apps/users/models/note.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-
 class Note(models.Model):
-    NOTE_TYPE_CHOICES = [
-        ("top", "Top"),
-        ("middle", "Middle"),
-        ("base", "Base"),
-    ]
+    class NoteType(models.TextChoices):
+        TOP = "top", "Top"
+        MIDDLE = "middle", "Middle"
+        BASE = "base", "Base"
+
     name = models.CharField(max_length=50)
-    type = models.CharField(max_length=10, choices=NOTE_TYPE_CHOICES)
+    type = models.CharField(max_length=10, choices=NoteType.choices)
 
     def __str__(self):
         return f"{self.get_type_display()} - {self.name}"
