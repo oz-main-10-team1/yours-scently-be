0f90dae2b10b9a20c744f08aa15ea30b5cb4ea70
jee1021 <dlawldnjs460@gmail.com>
2025-07-29 16:28:44 +0900

    ✨ 상품목록 모델 작성 (SECENTLY-32)
    프로덕트 모델작성 (SECENTLY-32)

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,5 @@
 # apps/products/urls.py
 from django.urls import path
-from .views import PerfumeListView
 
-urlpatterns = [
-    path('e-commerce/perfumes/', PerfumeListView.as_view(), name='perfume-list'),
-]
+
+urlpatterns = []
