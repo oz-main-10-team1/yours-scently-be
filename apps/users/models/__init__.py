e98bd5f59e2f10fa571feab681b7641daf071c0a
hoon <eoeo4652@naver.com>
2025-07-29 17:48:08 +0900

    ♻️ refactor : 리팩터링 (기능 변화 없음)
    istort/black/mypy 실행

--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -1,6 +1,6 @@
 from .accord import Accord
-from .fragrance_profile import Perfume
 from .fragrance_preference import FragrancePreference
+from .fragrance_profile import Perfume
 from .note import Note
 from .social_user import SocialUser
 from .user import User
