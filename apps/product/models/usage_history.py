f87fabf049e6e683e9fcd9ab2ab14e7f497d92d4
yeon tae <g9060g9060@gmail.com>
2025-07-31 00:53:22 +0900

    💡 chore: 모델 컬럼 수정 (SCENTLY-49)

--- apps/product/models/usage_history.py
+++ apps/product/models/usage_history.py
@@ -1,11 +1,14 @@
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class UsageHistory(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="usage_histories")
-    product = models.ForeignKey("product.Product", on_delete=models.CASCADE, related_name="usage_histories")
-    rating = models.IntegerField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="usage_histories"
+    )
+    product = models.ForeignKey("product.Product", on_delete=models.PROTECT, related_name="usage_histories")
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
