28cb78e2fd8fdaac78ddf190c7efffdb63353789
kyukyu300 <kyujin1252@gmail.com>
2025-07-31 03:03:52 +0900

    ✨ feat: like 모델 작성 (SCENTLY-51)

--- apps/product/models/__init__.py
+++ apps/product/models/__init__.py
@@ -1,5 +1,6 @@
+from .like import Like
 from .perfume import Perfume
 from .product import Product
 from .usage_history import UsageHistory
 
-__all__ = ["Product", "Perfume", "UsageHistory"]
+__all__ = ["Product", "Perfume", "UsageHistory", "Like"]
