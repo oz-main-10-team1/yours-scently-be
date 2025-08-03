fefadfad0a419da6871e40bbc5c0d25dff65a058
a <a@as-MacBook-Pro.local>
2025-08-03 23:11:52 +0900

    ✨ Feat:상품목록 조회 API-Urls.py에 Product_views.py 매핑(SCENTLY-26)

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from apps.product.views.like_list_view import ProductLikeListView
 from apps.product.views.like_views import ProductLikeAPIView
+from apps.product.views.product_views import ProductListView
 
 urlpatterns = [
     # 좋아요
     path("like/<int:product_id>/", ProductLikeAPIView.as_view(), name="product-like"),
     path("liked/products/", ProductLikeListView.as_view(), name="product-like-list"),
+
+    # 상품목록 조회
+    path("product/",ProductListView.as_view(), name="product-list"),
 ]
