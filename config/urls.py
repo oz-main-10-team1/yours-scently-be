534f14c5235164579a9422740bc90a86edacb45e
somy <fuureelf@naver.com>
2025-08-05 00:04:30 +0900

    ♻️refactor : 리팩터링 (SCENTLY-66)

--- config/urls.py
+++ config/urls.py
@@ -35,9 +35,8 @@ if settings.DEBUG:
         path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
         path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
         path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+        # 스웨거
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+        path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     ]
-    # 스웨거
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-]
