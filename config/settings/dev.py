066b5cd5501223ce18d94aca39ca9266564f6d45
kyukyu300 <kyujin1252@gmail.com>
2025-08-04 18:33:40 +0900

    🚚 build: 도메인 및 HTTPS 적용 (SCENTLY-72)

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,6 +1,6 @@
 from config.settings.base import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS: list[str] = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(" ")
 
 # Static files (CSS, JavaScript, Images)
