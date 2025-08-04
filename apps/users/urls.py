a4b4e8f393d8579b5e424b3b6fc5141b0de57b98
yeon tae <g9060g9060@gmail.com>
2025-08-04 16:48:50 +0900

    ♻️ refactor: url 수정 (SCENTLY-67)

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -38,9 +38,9 @@ urlpatterns = [
     # 회원 탈퇴
     path("deactivate/", WithdrawalAPIView.as_view(), name="user-withdrawal"),
     # 내 정보 조회
-    path("users/me", MyProfileView.as_view(), name="my-profile"),
+    path("users/me/", MyProfileView.as_view(), name="my-profile"),
     # 내 정보 수정
-    path("users/me/", MyProfileUpdateView.as_view(), name="my-profile-update"),
+    path("users/me/update/", MyProfileUpdateView.as_view(), name="my-profile-update"),
     # 사용자 취향 등록
     path("user/preference/", FragrancePreferenceCreateView.as_view(), name="create-fragrance-preference"),
 ]
