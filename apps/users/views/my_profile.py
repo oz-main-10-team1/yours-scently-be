faa447fc3afbe6ea5070318ee2777e6d1dbf4fdb
yeon tae <g9060g9060@gmail.com>
2025-08-05 11:35:48 +0900

    ✨ feat: 비밀번호 변경 API 작성 (SCENTLY-83)

--- apps/users/views/my_profile.py
+++ apps/users/views/my_profile.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.users.serializers.my_profile import (
+    ChangePasswordSerializer,
     MyProfileSerializer,
     MyProfileUpdateSerializer,
 )
@@ -30,3 +31,23 @@ class MyProfileUpdateView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request):
+        user = request.user
+        serializer = ChangePasswordSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+
+        new_password = serializer.validated_data["new_password"]
+        user.set_password(new_password)
+        user.save(update_fields=["password"])
+
+        return Response(
+            {
+                "message": "비밀번호가 변경되었습니다.",
+            },
+            status=status.HTTP_200_OK,
+        )
